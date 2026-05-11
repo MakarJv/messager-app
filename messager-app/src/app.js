e8be1e3f787f888af2694595688b00a34850b72c
messager-app <makar_igor1997@mail.ru>
2026-05-12 00:33:24 +0300

    update

--- messager-app/src/app.js
+++ messager-app/src/app.js
@@ -2,6 +2,13 @@ let loginBtn = document.querySelector('#login')
 let registerBtn = document.querySelector('#register')
 let login = document.querySelector('.login')
 let register = document.querySelector('.register')
+let enterBtn = document.querySelector('#enterBtn')
+let regBtn = document.querySelector('#regBtn')
+let chat = document.querySelector('.chat')
+let sendBtn = document.getElementById('sendBtn')
+let message = document.getElementById('message')
+let messageText = document.getElementById('messageText')
+
 
 registerBtn.onclick = function () {
     register.classList.remove('close')
@@ -11,3 +18,18 @@ loginBtn.onclick = function () {
     login.classList.remove('close')
     register.classList.add('close')
 }
+enterBtn.onclick = function () {
+    login.classList.add('close')
+    register.classList.add('close')
+    chat.classList.remove('close')
+}
+regBtn.onclick = function () {
+    login.classList.add('close')
+    register.classList.add('close')
+    chat.classList.remove('close')
+}
+sendBtn.addEventListener('click', function () {
+    let value = message.value
+    messageText.textContent = value
+    messageText.classList.add('messageText')
+})
